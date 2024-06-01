1e040c5889b3a1c23d59efd8545a6d0ffc51157f
sbx_user1051 <sbx_user1051@169.254.118.37>
2024-06-01 12:21:27 +0000

    Sat Jun 01 12:21:27 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,10 +1,12 @@
-from typing import Any
-
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name: Any) -> str:
-    if friend_name is None:
-        return 'hello'
-    return f'hello {friend_name}'
+def hello(*args) -> str:
+    # if not args:
+    #     return 'hello'
+    # if args[0] is None:
+    #     return 'hello'
+    # return f'hello {args[0]}`'
+    return "Hello, World!"
+
 
 
