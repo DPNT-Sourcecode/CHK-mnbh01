46d438196876e52cc056400da8f9e991371f743f
sbx_user1051 <sbx_user1051@169.254.118.37>
2024-06-01 12:17:58 +0000

    Sat Jun 01 12:17:58 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,7 +1,10 @@
-
+from typing import Any
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name: str) -> None:
+def hello(friend_name: Any) -> str:
+    if friend_name is None:
+        return 'hello'
     return f'hello {friend_name}'
+
 
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -1,11 +1,25 @@
 import unittest
 
+from lib.solutions.HLO.hello_solution import hello
+
 
 class HelloTest(unittest.TestCase):
 
     def test_hello(self):
-        assert True is True
+        for friend_name in (
+            'Jeremy',
+            'Kyle',
+            None,
+            1
+        ):
+            with self.subTest(f'{friend_name=}'):
+                result = hello(friend_name)
+                if friend_name is None:
+                    self.assertEqual(result, 'hello')
+                else:
+                    self.assertIn(str(friend_name), result)
 
 
 if __name__ == '__main__':
     unittest.main()
+
